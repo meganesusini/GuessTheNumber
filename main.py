91895dee8eb9e58aba2ee167e1c7e5cabbf6727b
megane <susini.megane04@gmail.com>
2023-02-17 13:26:06 +0100

    init

---
+++ main.py
@@ -0,0 +1,14 @@
+# The number to guess
+import random
+result = random.randint(0,10)
+print(result)
+
+print("The computer has selected a number.\n")
+nb = int(input("Saisis un nombre entier :"))
+
+# if(nb == result) :
+#     print("Right !")
+# elif (nb > result) :
+#     print("The number to guess is less than the number you wrote.")
+# else :
+#     print("The number to guess is higher than the number you wrote.")
